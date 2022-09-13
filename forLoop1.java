3f36ff6b39335ffb2b987c6263d62b6e6c4dbdb7
Shubham Kininge <shubhamkininge747@gmail.com>
2022-09-13 22:36:42 +0530

    Yes

---
+++ forLoop1.java
@@ -0,0 +1,13 @@
+public class forLoop1 {
+
+	public static void main(String[] args) {
+		int a=10;
+		System.out.println("Value of x from 0 to a");
+		for(int x=0;x<=a;x++)
+		{
+			System.out.println(x);
+		}
+
+	}
+
+}
