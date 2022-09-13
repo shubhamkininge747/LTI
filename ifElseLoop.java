31493e466b9afa7e93e0f3bdbfcf0afe990c9bea
Shubham Kininge <shubhamkininge747@gmail.com>
2022-09-13 19:44:19 +0530

    YOO

---
+++ ifElseLoop.java
@@ -0,0 +1,14 @@
+
+public class ifElseLoop {
+
+	public static void main(String[] args) {
+		int age=17;
+		if(age>18)
+		{
+			System.out.println("Eligible for PanCard");
+		}
+		else
+			System.out.println("Not eligible");
+	}
+
+}
